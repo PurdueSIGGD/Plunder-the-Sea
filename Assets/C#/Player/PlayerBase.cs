82e02013e4bd348a2232207818e27d0ddf3e8514
Clayton <cjdetke@yahoo.com>
2020-09-23 17:25:01 -0400

    Base implementation

---
+++ Assets/C#/Player/PlayerBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBase : MonoBehaviour
+{
+    [HideInInspector]
+    public Rigidbody2D myRigid;
+    [HideInInspector]
+    public PlayerMovement myMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMovement = (PlayerMovement)GetComponent<PlayerMovement>();
+        myRigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
---
+++ Assets/C#/Player/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [HideInInspector]
+    public PlayerBase myBase;
+
+    private float movementSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBase = (PlayerBase)GetComponent<PlayerBase>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        myBase.myRigid.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * movementSpeed, Input.GetAxisRaw("Vertical") * movementSpeed);
+    }
+}
