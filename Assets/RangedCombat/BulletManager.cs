f5374b6a2b57004a2a30f60812683b9fc3a3cca1
etschymik <70454939+etschymik@users.noreply.github.com>
2020-09-23 20:56:10 -0400

    Basic ranged combat

---
+++ Assets/RangedCombat/BulletManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour
+{
+    float lifeTime = 2;
+    float currentLifeTime = 0;
+    public Vector2 velocity;
+    public GameObject origin;
+
+    void Update()
+    {
+        transform.position += (Vector3) (velocity * Time.deltaTime);
+
+        currentLifeTime += Time.deltaTime;
+        if(currentLifeTime >= lifeTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/RangedCombat/RangedCombat.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedCombat : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+ 
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            var bullet = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+            
+        }
+    }
+}
