7328eb14152270ad3433ca6e2e4f4ef0941093d9
Landon Ellis <landon.ellis2@gmail.com>
2020-09-24 23:28:25 -0400

    Mouse Aiming & Shoot function

---
+++ Assets/C#/RangedCombat/BulletManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BulletManager : MonoBehaviour
+{
+    float lifeTime = 2;
+    float currentLifeTime = 0;
+    private Rigidbody2D rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        currentLifeTime += Time.deltaTime;
+        if(currentLifeTime >= lifeTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public static BulletManager Shoot(GameObject prefab, GameObject source, Vector2 target, float speed)
+    {
+        BulletManager bullet = Shoot(prefab, source.transform.position, target, speed);
+        return bullet;
+    }
+
+    public static BulletManager Shoot(GameObject prefab, Vector2 startPos, Vector2 target, float speed)
+    {
+
+        BulletManager bullet = Instantiate(prefab, startPos, Quaternion.identity).GetComponent<BulletManager>();
+        if (!bullet)
+        {
+            return null;
+        }
+
+        Vector2 direction = (target - startPos).normalized;
+        bullet.GetComponent<Rigidbody2D>().velocity = direction * speed;
+        bullet.transform.rotation = Quaternion.FromToRotation(Vector3.right, new Vector3(direction.x, direction.y, 0));
+
+        return bullet;
+
+    }
+}
---
+++ Assets/C#/RangedCombat/RangedCombat.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedCombat : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    public float projectileSpeed = 5.0f;
+    private Camera cam;
+
+    private void Start()
+    {
+        /* Assuming one camera */
+        cam = GameObject.FindObjectOfType<Camera>();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+
+            Vector3 mouseWorldPos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane));
+            BulletManager.Shoot(projectilePrefab, gameObject, mouseWorldPos, projectileSpeed);
+
+        }
+    }
+}
