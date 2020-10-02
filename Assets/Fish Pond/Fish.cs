00377f6b7e46df080664d82dc00d3e3735f25107
etschymik <70454939+etschymik@users.noreply.github.com>
2020-10-02 13:06:14 -0400

    The fish pond and player fishing
    Not complete. Need to work on collision system with bobber and fish to work with either DDR or reeling in fish.

---
+++ Assets/Fish Pond/Fish.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    public float speed;
+    public float randomX;
+    public float randomY;
+    public float minWaitTime;
+    public float maxWaitTime;
+    private Vector3 randomPos;
+
+    void Start()
+    {
+        RandomPosition();
+    }
+
+    // finds random position for fish
+    void RandomPosition()
+    {
+        randomPos = new Vector3(Random.Range(-randomX, randomX), Random.Range(-randomY, randomY), 0);
+        StartCoroutine(MoveToRandomPos());
+    }
+
+    // moves the fish to the random location from the current position
+    IEnumerator MoveToRandomPos()
+    {
+        float i = 0.0f;
+        float rate = 1.0f / speed;
+        Vector3 currentPos = transform.position;
+
+        // moves fish to random location
+        while (i < 1.0f)
+        {
+            i += Time.deltaTime * rate;
+            transform.position = Vector3.Lerp(currentPos, randomPos, i);
+            yield return null;
+        }
+
+        float randomFloat = Random.Range(0.0f, 1.0f);
+        if (randomFloat < 0.5f)
+            StartCoroutine(WaitBetweenMovements());
+        else
+            RandomPosition();
+    }
+
+    // fish waits at position if called then chooses another random location
+    IEnumerator WaitBetweenMovements()
+    {
+        yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+        RandomPosition();
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        // if it hits a wall then stop and go the other direction
+        if(col.gameObject.tag == "Pond")
+        {
+            StopAllCoroutines();
+            randomPos = -randomPos/2 + new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
+            StartCoroutine(MoveToRandomPos());
+        }
+
+        // fill with things to do when fish is caught
+        else if(col.gameObject.tag == "Bobber")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Fish Pond/PlayerFishing.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFishing : MonoBehaviour
+{
+    public GameObject bobberSprite;
+    public float castingSpd;
+    bool casted = false;
+    public bool inWater = false;
+    GameObject bobber;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cast Fishing Pole"))   // F key
+        {
+            if (casted == false)
+                StartCoroutine(Cast());
+            else
+                StartCoroutine(ReelIn());
+        }
+    }
+
+    // casts the bobber to the point of the mouse
+    public IEnumerator Cast()
+    {
+        casted = true;
+        bobber = Instantiate(bobberSprite, transform.position, Quaternion.identity);
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
+        float t = 0.0f;
+        while (t < 1.0f)
+        {
+            t += Time.deltaTime * castingSpd;
+            bobber.transform.position = Vector2.Lerp(transform.position, mousePos, t);
+            yield return null;
+        }
+        inWater = true;
+    }
+
+    // returns the bobber to the player position and destroys it
+    public IEnumerator ReelIn()
+    {
+        inWater = false;
+        float t = 0.0f;
+        while (t < 1.0f)
+        {
+            t += Time.deltaTime * castingSpd;
+            bobber.transform.position = Vector2.Lerp(bobber.transform.position, transform.position, t);
+            yield return null;
+        }
+        casted = false;
+        Destroy(bobber);
+    }
+}
