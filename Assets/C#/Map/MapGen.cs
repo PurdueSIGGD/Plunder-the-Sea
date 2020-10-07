71b8bb564e8cd103051eafffd958fec916258eb1
Phoebus Yang <yang1339@purdue.edu>
2020-10-07 19:07:12 -0400

    Create map gen script

---
+++ Assets/C#/Map/MapGen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject room;
+
+    void Start()
+    {
+        Object.Instantiate(room, Vector3.zero, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
