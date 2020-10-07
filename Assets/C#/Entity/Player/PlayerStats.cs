70296ad770b3f427c134965c28b61ddd027b3437
Clayton <cjdetke@yahoo.com>
2020-10-07 18:29:52 -0400

    Added stamina bar UI
    Also mostly fixed stamina implementation for melee combat.

--- Assets/C#/Entity/Player/PlayerStats.cs
+++ Assets/C#/Entity/Player/PlayerStats.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerStats : EntityStats
 {
 
     PlayerBase pbase;
+    public float staminaMax = 100;
+    public float stamina = 100;
+    public float staminaRechargeRate = 2f;
+    public Slider staminaBar;
+    private float timeSinceLastTick = 0;
+    private float timeBetweenTicks = 0.1f;
 
     private void Start()
     {
         pbase = GetComponent<PlayerBase>();
+    }
+
+    private void Update()
+    {
+        staminaBar.value = stamina / staminaMax;
+        if(Time.time > timeSinceLastTick + timeBetweenTicks)
+        {
+            timeSinceLastTick = Time.time;
+            stamina = Mathf.Min(stamina + staminaRechargeRate, staminaMax);
+        }
+    }
+
+    public void UseStamina(float staminaCost)
+    {
+        stamina = Mathf.Max(stamina - staminaCost, 0);
     }
 
     public override void Die()
