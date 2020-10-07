38ee307c11f15a4e1cdb1a035dd1fc5188ac3ac1
Andrew Choi <kimchy101@gmail.com>
2020-10-07 18:34:32 -0400

    Added chests that grant weapons

--- Assets/WeaponInventory.cs
+++ Assets/WeaponInventory.cs
@@ -7,16 +7,9 @@ public class WeaponInventory : MonoBehaviour
     public ScriptableWeapon meleeWeapon;
     public ScriptableWeapon rangeWeapon;
 
-    public ScriptableWeapon testWeaponSlot;
-
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            var rangeWeaponStart = rangeWeapon;
-            rangeWeapon = testWeaponSlot;
-            testWeaponSlot = rangeWeaponStart;
-        }
-
+        
     }
+
 }
