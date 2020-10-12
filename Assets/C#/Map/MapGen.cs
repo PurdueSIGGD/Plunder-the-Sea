cfeb4e91d259f225fa4b6947318e29f4ebc5bd5a
Phoebus Yang <yang1339@purdue.edu>
2020-10-12 18:51:49 -0400

    Fill in unused room exits

--- Assets/C#/Map/MapGen.cs
+++ Assets/C#/Map/MapGen.cs
@@ -12,6 +12,7 @@ public class MapGen : MonoBehaviour
     public const int ROOMDIST = 6;
     public GameObject room;
     public GameObject hall;
+    public GameObject wall;
     public int truePathLength;
 
     private Hashtable roomGrid;
@@ -22,7 +23,7 @@ public class MapGen : MonoBehaviour
         public int x;
         public int y;
         public List<int> nextDirs;
-        public List<int> connectDirs;
+        public bool[] connectDirs;
         public RoomData parent;
 
         public RoomData(int x, int y, RoomData p)
@@ -31,7 +32,7 @@ public class MapGen : MonoBehaviour
             this.y = y;
             nextDirs = new List<int>(new int[] {0 ,1, 2, 3});
             parent = p;
-            connectDirs = new List<int>();
+            connectDirs = new bool[4];
         }
     }
 
@@ -64,8 +65,8 @@ public class MapGen : MonoBehaviour
                 RoomData nextRoom = new RoomData(x+dx, y+dy, curr);
                 roomGrid.Add((nextRoom.x, nextRoom.y), nextRoom);
                 roomStack.Push(nextRoom);
-                curr.connectDirs.Add(rawDir);
-                nextRoom.connectDirs.Add((rawDir+2) % 4);
+                curr.connectDirs[rawDir] = true;
+                nextRoom.connectDirs[(rawDir+2) % 4] = true;
                 x = nextRoom.x;
                 y = nextRoom.y;
                 curr = nextRoom;
@@ -94,17 +95,37 @@ public class MapGen : MonoBehaviour
     public void buildRoom(RoomData newRoom)
     {
         int roomScale = ROOMWIDTH + ROOMDIST;
-        foreach (int rawDir in newRoom.connectDirs)
+        for (int i = 0; i < 4; i++)
         {
-            //spawn hallways
-            (int, int) dir = toDirection(rawDir);
-            (float, float) hallLoc = (newRoom.x + .5f * dir.Item1, newRoom.y + .5f * dir.Item2);
-            if (!roomGrid.Contains(hallLoc))
+            (int, int) dir = toDirection(i);
+            if (newRoom.connectDirs[i])
             {
-                Debug.Log("Hall: "+hallLoc);
-                roomGrid.Add(hallLoc, 1);
+                //spawn hallways
+                (float, float) hallLoc = (newRoom.x + .5f * dir.Item1, newRoom.y + .5f * dir.Item2);
+                if (!roomGrid.Contains(hallLoc))
+                {
+                    Debug.Log("Hall: " + hallLoc);
+                    roomGrid.Add(hallLoc, 1);
+                    Quaternion rot;
+                    if (dir.Item1 == 0)
+                    {
+                        rot = Quaternion.identity;
+                    }
+                    else
+                    {
+                        rot = Quaternion.Euler(0, 0, 90);
+                    }
+                    Object.Instantiate(hall, new Vector3(hallLoc.Item1 * roomScale + .5f * ROOMWIDTH,
+                        hallLoc.Item2 * roomScale + .5f * ROOMWIDTH, 0), rot);
+                }
+            }
+            else
+            {
+                //plug opening
+                (float, float) wallLoc = (newRoom.x * roomScale + .5f * ((dir.Item1 + 1) * ROOMWIDTH - dir.Item1),
+                    newRoom.y * roomScale + .5f * ((dir.Item2 + 1) * ROOMWIDTH - dir.Item2));
                 Quaternion rot;
-                if (dir.Item1 == 0)
+                if (dir.Item2 == 0)
                 {
                     rot = Quaternion.identity;
                 }
@@ -112,8 +133,7 @@ public class MapGen : MonoBehaviour
                 {
                     rot = Quaternion.Euler(0, 0, 90);
                 }
-                Object.Instantiate(hall, new Vector3(hallLoc.Item1 * roomScale + .5f * ROOMWIDTH,
-                    hallLoc.Item2 * roomScale + .5f * ROOMWIDTH, 0), rot);
+                Object.Instantiate(wall, new Vector3(wallLoc.Item1, wallLoc.Item2, 0), rot);
             }
         }
         Debug.Log("Room: " + (newRoom.x,newRoom.y));
