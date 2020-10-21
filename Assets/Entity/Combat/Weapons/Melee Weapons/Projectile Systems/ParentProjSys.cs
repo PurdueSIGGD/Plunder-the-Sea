06385137ef33c14a559694c43068a699e18a8c79
Landon Ellis <landon.ellis2@gmail.com>
2020-10-21 18:23:17 -0400

    Cleanup: Added parenting & DontDestroyOnCollide to all melee weapons

--- Assets/Entity/Combat/Weapons/Melee Weapons/Projectile Systems/ParentProjSys.cs
+++ Assets/Entity/Combat/Weapons/Melee Weapons/Projectile Systems/ParentProjSys.cs
@@ -3,15 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ParentProjSys", menuName = "ScriptableObjects/ProjectileSystems/Parent", order = 1)]
 public class ParentProjSys : ProjectileSystem {
 
-    private Transform originTrans;
-
     public override void OnFire(Projectile projectile)
     {
-        projectile.transform.SetParent(originTrans);
-    }
-
-    public override void OnEquip(WeaponInventory inv)
-    {
-        originTrans = inv.transform;
+        projectile.transform.SetParent(projectile.source.transform);
     }
 }
--- Assets/Entity/Combat/Weapons/Melee Weapons/Projectile Systems/StaminaProjSys.cs
+++ Assets/Entity/Combat/Weapons/Melee Weapons/Projectile Systems/StaminaProjSys.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "StaminaProjSys", menuName = "ScriptableObjects/ProjectileSystems/Stamina", order = 1)]
 public class StaminaProjSys : ProjectileSystem {
     [SerializeField]
-    private float staminaCost;
+    private float staminaCost = 1.0f;
 
     private PlayerStats stats;
 
