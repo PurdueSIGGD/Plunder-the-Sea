901da29d7838774458864d01b4b07f840634d61a
Clayton <cjdetke@yahoo.com>
2020-10-23 18:45:38 -0400

    Further fish AI update
    Now just need to make fish bounce off of walls

--- Assets/Fishing/Fish/Fish.cs
+++ Assets/Fishing/Fish/Fish.cs
@@ -16,12 +16,12 @@ public class Fish : MonoBehaviour
     public float buffStaminaRechargeRate = 0f;
     public int lootLevel;
     public GameObject FishingMinigame;
-    private const float rotationSpeed = 0.3f;
+    private const float rotationSpeed = 0.1f;
 
     void Start()
     {
         RandomPosition();
-        transform.eulerAngles = new Vector3(0, 0, Random.Range(-180f, 180f));
+        transform.eulerAngles = new Vector3(0, 0, Random.Range(0f, 360f));
     }
 
     public void BuffPlayerStats(PlayerStats stats)
@@ -36,17 +36,32 @@ public class Fish : MonoBehaviour
     void RandomPosition()
     {
         randomPos = new Vector3(Random.Range(-randomX, randomX), Random.Range(-randomY, randomY), 0);
-        StartCoroutine(MoveToRandomPos());
+        StartCoroutine(MoveRandomly());
     }
 
     void IncrementRotation(Vector3 newRotation)
     {
+        while (true) // This makes the target angle be within 180 degrees of where the fish is facing.
+        {
+            if(transform.eulerAngles.z + 180f < newRotation.z)
+            {
+                newRotation.z -= 360f;
+            }
+            else if(transform.eulerAngles.z - 180f > newRotation.z)
+            {
+                newRotation.z += 360f;
+            }
+            else
+            {
+                break;
+            }
+        }
         float z = newRotation.z;
         if(transform.eulerAngles.z > z)
         {
-            if(transform.eulerAngles.z > z + rotationSpeed)
+            if(transform.eulerAngles.z > z + (speed * rotationSpeed))
             {
-                transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z - rotationSpeed);
+                transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z - (speed * rotationSpeed));
             }
             else
             {
@@ -55,9 +70,9 @@ public class Fish : MonoBehaviour
         }
         else
         {
-            if(transform.eulerAngles.z < z - rotationSpeed)
+            if(transform.eulerAngles.z < z - (speed * rotationSpeed))
             {
-                transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z + rotationSpeed);
+                transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z + (speed * rotationSpeed));
             }
             else
             {
@@ -67,20 +82,16 @@ public class Fish : MonoBehaviour
     }
 
     // moves the fish to the random location from the current position
-    IEnumerator MoveToRandomPos()
+    IEnumerator MoveRandomly()
     {
         float i = 0.0f;
-        Vector3 currentPos = transform.position;
-        Vector3 newPos;
-        float angle;
+        float angle = transform.eulerAngles.z + Random.Range(-90f, 90f);
 
         // moves fish to random location
         while (i < 1.0f)
         {
-            i += Time.deltaTime * speed;
-            newPos = Vector3.Lerp(currentPos, randomPos, i);
-            angle = Mathf.Atan2(transform.position.y - newPos.y, transform.position.x - newPos.x) * Mathf.Rad2Deg;
-            IncrementRotation(Quaternion.AngleAxis(angle, Vector3.forward).eulerAngles);
+            i += Time.deltaTime * (speed / 3);
+            IncrementRotation(new Vector3(0, 0, angle));
             transform.position += transform.up * Time.deltaTime * speed;
             yield return null;
         }
@@ -106,7 +117,7 @@ public class Fish : MonoBehaviour
         {
             StopAllCoroutines();
             randomPos = -randomPos/2 + new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
-            StartCoroutine(MoveToRandomPos());
+            StartCoroutine(MoveRandomly());
         }
     }
 }
