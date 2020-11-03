95142b9793db104478b7b325f2db040452c78bfe
andyjsharpe <48738245+andyjsharpe@users.noreply.github.com>
2020-11-02 19:05:39 -0500

    Basic stuff
    Created a script for use with storing/giving class data. It currently only modifies the player speed.

---
+++ Assets/Entity/Player/PlayerClasses.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClasses : MonoBehaviour
+{
+    public float attackMultiplier = 1f;
+    public float speedModifier = 0f;
+
+    private void Start()
+    {
+        GetComponent<PlayerStats>().movementSpeed += speedModifier;
+    }
+
+    public struct weaponMods{
+        //code here
+    }
+
+}
