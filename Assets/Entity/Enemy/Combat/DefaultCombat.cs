dd373f189c24a027871ceca3f3cf45e7424ff9cc
Tom Yarrow <tyarrow1@gmail.com>
2020-11-11 19:14:39 -0500

    Default enemy behavior restored (they can deal damage now)

---
+++ Assets/Entity/Enemy/Combat/DefaultCombat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultCombat : EnemyCombat
+{
+    // Script used for default enemy's combat
+    private void OnTriggerEnter2D(Collider2D collider) //Called when something enters the enemy's range
+    {
+        if (collider.GetComponent<PlayerBase>())
+        {
+            myBase.myMovement.moving = false;
+            myBase.myRigid.velocity = Vector2.zero;
+            StartCoroutine("meleeAttack");
+        }
+    }
+    IEnumerator meleeAttack() //Executes a melee attack
+    {
+        yield return new WaitForSeconds(1 / myBase.myStats.attackSpeedInverse);
+        if (Vector3.Distance(transform.position, myBase.player.transform.position) <= attackRange)
+        {
+            myBase.player.GetComponent<PlayerBase>().stats.TakeDamage(myBase.myStats.damage);
+        }
+        myBase.myMovement.moving = true;
+    }
+}
