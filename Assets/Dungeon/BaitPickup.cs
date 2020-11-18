36fb6dd948eb2cbd62473b16ce9535b1bc72137b
TheNerdv2 <mbeshear@purdue.edu>
2020-11-18 18:41:02 -0500

    Added Bait and Health pickups
    Added scripts and basic prefabs for each

---
+++ Assets/Dungeon/BaitPickup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaitPickup : MonoBehaviour
+{
+    public int baitType = 0;
+    public int baitAmount = 1;
+
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        PlayerStats player = collider.GetComponent<PlayerStats>();
+        if (player == null)
+        {
+            return; //Not a player
+        }
+
+        //Update player bait
+        player.addBait(baitType, baitAmount);
+
+        //Destory bait pickup
+        GameObject.Destroy(transform.gameObject);
+    }
+
+}
+
---
+++ Assets/Dungeon/HealthPickup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour
+{
+    public float health = 1.0f;
+
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        PlayerStats player = collider.GetComponent<PlayerStats>();
+        if (player == null)
+        {
+            return; //Not a player
+        }
+
+        //If player is at full health
+        if (player.currentHP == player.maxHP)
+        {
+            return; //Player at full health
+        }
+
+        //Update player health
+        player.currentHP = Math.Min(player.currentHP + health, player.maxHP);
+
+        //Destory health pickup
+        GameObject.Destroy(transform.gameObject);
+    }
+
+}
