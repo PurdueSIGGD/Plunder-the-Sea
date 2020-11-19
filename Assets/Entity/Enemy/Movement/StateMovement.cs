9b07247eaab938897f14bd76de64c88b025aa533
Phoebus Yang <yang1339@purdue.edu>
2020-11-18 20:10:37 -0500

    Implemented A* based pathing and added diagonal movement to pathing

--- Assets/Entity/Enemy/Movement/StateMovement.cs
+++ Assets/Entity/Enemy/Movement/StateMovement.cs
@@ -68,33 +68,42 @@ public class StateMovement : EnemyMovement
     {
         Hashtable pathMap = new Hashtable();
         LinkedList<MoveAction> moveActionsBuild = new LinkedList<MoveAction>();
-        Queue<PathAction> frontier = new Queue<PathAction>();
+        //Queue<PathAction> frontier = new Queue<PathAction>();
+        PriorityQueue frontier = new PriorityQueue();
         Vector2 myPos = centerVector(myBase.myRigid.position);
         Vector2 playerPos = centerVector(myBase.player.transform.position);
-        pathMap.Add(myPos, true);
-        frontier.Enqueue(new PathAction(new Vector2(myPos.x, myPos.y), null));
+        PathAction startAct = new PathAction(new Vector2(myPos.x, myPos.y), null);
+        pathMap.Add(myPos, startAct);
+        //frontier.Enqueue(startAct);
+        frontier.Push(startAct);
         ContactFilter2D filter = new ContactFilter2D();
         filter.SetLayerMask(1 << 8);
         //Debug.Log("Start: " + myPos);
 
-        while (frontier.Count > 0)
+        while (frontier.Count() > 0)
         {
-            PathAction curr = frontier.Dequeue();
+            PathAction curr = frontier.Pop();
             if (Mathf.Abs(curr.pos.x - playerPos.x) <= .1 && Mathf.Abs(curr.pos.y - playerPos.y) <= .1)
             {
                 while (curr.parent != null)
                 {
-                    moveActionsBuild.AddFirst(new MoveAction(curr.pos - curr.parent.pos, 1));
+                    //Debug.Log(curr.pos + " value: "+curr.getValue() + " dist: " + curr.dist);
+                    moveActionsBuild.AddFirst(new MoveAction(curr.pos - curr.parent.pos, (curr.pos - curr.parent.pos).magnitude));
                     curr = curr.parent;
                 }
                 break;
             }
             if (Vector2.Distance(curr.pos, playerPos) < maxDist)
             {
-                explorePath(Vector2.up, curr, frontier, pathMap, filter);
-                explorePath(Vector2.right, curr, frontier, pathMap, filter);
-                explorePath(Vector2.down, curr, frontier, pathMap, filter);
-                explorePath(Vector2.left, curr, frontier, pathMap, filter);
+                explorePath(Vector2.right + Vector2.up, playerPos, curr, frontier, pathMap, filter);
+                explorePath(Vector2.right + Vector2.down, playerPos, curr, frontier, pathMap, filter);
+                explorePath(Vector2.left + Vector2.up, playerPos, curr, frontier, pathMap, filter);
+                explorePath(Vector2.left + Vector2.down, playerPos, curr, frontier, pathMap, filter);
+
+                explorePath(Vector2.up, playerPos, curr, frontier, pathMap, filter);
+                explorePath(Vector2.right, playerPos, curr, frontier, pathMap, filter);
+                explorePath(Vector2.down, playerPos, curr, frontier, pathMap, filter);
+                explorePath(Vector2.left, playerPos, curr, frontier, pathMap, filter);
             }
 
 
@@ -103,16 +112,31 @@ public class StateMovement : EnemyMovement
 
     }
 
-    private void explorePath(Vector2 dir, PathAction parent, Queue<PathAction> front, Hashtable map, ContactFilter2D filter)
+    private void explorePath(Vector2 dir, Vector2 target, PathAction parent, PriorityQueue front, Hashtable map, ContactFilter2D filter)
     {
         RaycastHit2D[] hits = new RaycastHit2D[1];
         Vector2 newPos = parent.pos + dir;
+        //Debug.Log(parent.pos + " -> " + newPos);
         if (!map.Contains(newPos))
         {
-            if (Physics2D.Raycast(parent.pos, dir, filter, hits, 1.1f) <= 0)
+            if (Physics2D.Raycast(parent.pos, dir, filter, hits, dir.magnitude+.1f) <= 0)
             {
-                map.Add(newPos, true);
-                front.Enqueue(new PathAction(newPos, parent));
+                PathAction newAct = new PathAction(newPos, parent, target);
+                map.Add(newPos, newAct);
+                front.Push(newAct);
+            }
+        }
+        else
+        {
+            //check if should update value in frontier
+            if (Physics2D.Raycast(parent.pos, dir, filter, hits, dir.magnitude+.1f) <= 0)
+            {
+                PathAction oldAct = (PathAction)map[newPos];
+                if (parent.dist + dir.magnitude < oldAct.dist)
+                {
+                    oldAct.parent = parent;
+                    front.Update(oldAct, parent.dist + dir.magnitude);
+                }
             }
         }
     }
@@ -166,14 +190,154 @@ public class MoveAction
     }
 }
 
+public class PriorityQueue
+{
+    public List<PathAction> queue;
+
+    public PriorityQueue()
+    {
+        queue = new List<PathAction>();
+    }
+
+    public int Count()
+    {
+        return queue.Count;
+    }
+
+    public void Update(PathAction act, float newDist)
+    {
+        //binary search to find action in queue
+        int high = queue.Count - 1;
+        float highVal = queue[high].getValue();
+        int low = 0;
+        float lowVal = queue[low].getValue();
+        float actVal = act.getValue();
+
+        while (highVal > lowVal)
+        {
+            int mid = (high + low) / 2;
+            float midVal = queue[mid].getValue();
+            if (midVal >= actVal)
+            {
+                high = mid;
+                highVal = midVal;
+            }
+            else
+            {
+                low = mid+1;
+                lowVal = queue[low].getValue();
+            }
+        }
+
+        int curr = low;
+        float currVal = lowVal;
+        while (currVal <= highVal && curr < queue.Count)
+        {
+            if (act == queue[curr])
+            {
+                queue.RemoveAt(curr);
+                break;
+            }
+            curr++;
+            currVal = queue[curr].getValue();
+        }
+
+        act.dist = newDist;
+        Push(act);
+    }
+
+    public void Push(PathAction act)
+    {
+        if (queue.Count <= 0)
+        {
+            queue.Add(act);
+            return;
+        }
+        int high = queue.Count-1;
+        int low = 0;
+        float actVal = act.getValue();
+
+        //Binary search for insertion location
+        while (high - low > 1)
+        {
+            int mid = (high + low) / 2;
+            if (queue[mid].getValue() > actVal)
+            {
+                high = mid;
+            }
+            else
+            {
+                low = mid;
+            }
+        }
+        if (queue[low].getValue() > actVal)
+        {
+            queue.Insert(low, act);
+        }
+        else if (queue[high].getValue() < actVal)
+        {
+            queue.Insert(high + 1, act);
+        }
+        else
+        {
+            queue.Insert(low + 1, act);
+        }
+    }
+
+    public PathAction Peek()
+    {
+        return queue[0];
+    }
+    public PathAction Pop()
+    {
+        PathAction ret = queue[0];
+        queue.RemoveAt(0);
+        return ret;
+    }
+
+}
+
 public class PathAction
 {
     public Vector2 pos;
     public PathAction parent;
+    public float dist = 0;
+    public float heuristic = 0;
+
+    public PathAction(Vector2 pos, PathAction parent, Vector2 target, float nextDist)
+    {
+        this.pos = pos;
+        this.parent = parent;
+        if (parent != null)
+        {
+            dist = parent.dist + nextDist;
+        }
+        heuristic = Vector2.Distance(pos, target);
+    }
+
+    public PathAction(Vector2 pos, PathAction parent, Vector2 target)
+    {
+        this.pos = pos;
+        this.parent = parent;
+        if (parent != null)
+        {
+            dist = parent.dist + Vector2.Distance(parent.pos, pos);
+        }
+        heuristic = Vector2.Distance(pos, target);
+    }
 
     public PathAction(Vector2 pos, PathAction parent)
     {
         this.pos = pos;
         this.parent = parent;
+        if (parent != null)
+        {
+            dist = parent.dist + Vector2.Distance(parent.pos, pos);
+        }
+    }
+
+    public float getValue()
+    {
+        return dist + heuristic;
     }
 }
