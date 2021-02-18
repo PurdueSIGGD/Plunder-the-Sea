f9968e318793c919b745dec29ea71e09b85ecf7d
Tom Yarrow <tyarrow1@gmail.com>
2021-02-17 20:02:17 -0500

    Beginning sprite changes, Random Sprite (and explosion variants)

---
+++ Assets/Entity/Enemy/RandomSprite.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSprite : MonoBehaviour
+{
+    public Sprite[] sprites;            // The sprites to choose from
+    public GameObject destination;      // The destination to give the sprite to
+
+    private void Awake()
+    {
+        destination.GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
+    }
+}
