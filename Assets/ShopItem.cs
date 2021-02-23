5a1410f5740e18a827668e17ecc8daad158bf048
Andrew Choi <kimchy101@gmail.com>
2021-02-22 20:02:31 -0500

    weapon shop generator implemented

--- Assets/ShopItem.cs
+++ Assets/ShopItem.cs
@@ -8,11 +8,37 @@ public class ShopItem : MonoBehaviour
     private GameObject chestPrefab;
     [SerializeField]
     private WeaponFactory.CLASS weaponClass;
+    [SerializeField]
+    private WeaponTables tables;
     private bool used = false;
     [SerializeField]
     private int baitPrice;
     [SerializeField]
     private int baitType;
+
+    [SerializeField]
+    private SpritesWeaponClassTable rangedWeaponSpritesTable; 
+    [SerializeField]
+    private PrefabWeaponClassTable projectilePrefabTable; 
+
+    public void SetShopItem(WeaponFactory.CLASS weaponClass, int baitPrice, int baitType) {
+        this.weaponClass = weaponClass;
+        this.baitPrice = baitPrice;
+        this.baitType = baitType;
+    }
+
+    public void OnEnable() {
+        var newTMPText = "T: " + baitType + " P: " + baitPrice;
+        GetComponentInChildren<TMPro.TMP_Text>().text = newTMPText;
+
+        Sprite newSprite = null;
+        if (tables.tagWeapon.get(weaponClass) == WeaponFactory.TAG.MELEE) {
+            newSprite = projectilePrefabTable.get(weaponClass).GetComponentInChildren<SpriteRenderer>().sprite;
+        } else {
+            newSprite = rangedWeaponSpritesTable.get(weaponClass);
+        }
+        GetComponentInChildren<SpriteRenderer>().sprite = newSprite;
+    }
 
     public void OnTriggerEnter2D(Collider2D collider)
     {
