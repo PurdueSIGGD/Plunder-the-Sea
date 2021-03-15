e667d8e62ffaab406bb783f28d2aaa4d61848c51
Tom Yarrow <tyarrow1@gmail.com>
2021-03-15 19:49:12 -0400

    Dragon fish body has strong damage reduction

---
+++ Assets/Entity/Enemy/Combat/ApplyAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyAttribute : MonoBehaviour
+{
+    // Unique script that just gives an entity a permanent attribute
+    public EntityStats source;
+    public EntityAttribute attr;
+
+    void Start()
+    {
+        source.AddAttribute(attr, source);
+    }
+}
