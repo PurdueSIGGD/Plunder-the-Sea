e8a3045774e95b3a0945d46ee5a9c558be0bbf61
yang132 <yang1339@purdue.edu>
2021-03-29 20:04:47 -0400

    Set up table for weapon description

---
+++ Assets/Entity/Combat/Weapons/Tables/Scripts/DescrWeaponClassTable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "DescrWeaponClassTable", menuName = "ScriptableObjects/Table/DescrWeaponClass", order = 1)]
+public class DescrWeaponClassTable : ScriptableObject
+{
+    // DO NOT MODIFY THE PAIR CLASS
+    [System.Serializable]
+    public class Pair
+    {
+        public WeaponFactory.CLASS weaponClass;
+        [TextArea]
+        public string description;
+    }
+    public Pair[] classDescrPairs;
+    public string get(WeaponFactory.CLASS weaponClass)
+    {
+        return System.Array.Find(this.classDescrPairs, (p) => p.weaponClass == weaponClass).description;
+    }
+}
