29da55a33118e164fbfe06131e216d93d28f8751
Andrew Choi <kimchy101@gmail.com>
2021-04-05 19:30:22 -0400

    fixed cave teleporter issue

--- Assets/Overworld/caveTeleporter.cs
+++ Assets/Overworld/caveTeleporter.cs
@@ -6,10 +6,11 @@ public class caveTeleporter : MonoBehaviour
 {
 
     [SerializeField]
-    private Vector3 teleportLocation;
+    private Transform teleportTransform;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        var teleportLocation = teleportTransform.position;
         if (collision.gameObject.CompareTag("Player"))
         {
             parrot p = FindObjectOfType<parrot>();
