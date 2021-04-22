876c632c69440165839577158138c329f5da3cdc
andyjsharpe <48738245+andyjsharpe@users.noreply.github.com>
2021-04-21 21:35:31 -0400

    Basic enemy Projectile sounds

---
+++ Assets/dupeAudio.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dupeAudio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject g = Instantiate(new GameObject(), transform.position, Quaternion.identity);
+        AudioSource thisAS = GetComponent<AudioSource>();
+        AudioSource newAS = g.AddComponent<AudioSource>();
+
+        newAS.clip = thisAS.clip;
+        newAS.volume = thisAS.volume;
+        newAS.pitch = thisAS.pitch;
+        newAS.spatialBlend = thisAS.spatialBlend;
+        newAS.rolloffMode = thisAS.rolloffMode;
+        newAS.minDistance = thisAS.minDistance;
+        newAS.maxDistance = thisAS.maxDistance;
+
+        Destroy(thisAS);
+        newAS.Play();
+        Destroy(g, newAS.clip.length + 0.1f);
+    }
+}
