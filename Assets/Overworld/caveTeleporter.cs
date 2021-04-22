e46aba33985f23c2e315160eb36d23739f13cb48
andyjsharpe <48738245+andyjsharpe@users.noreply.github.com>
2021-04-21 22:45:20 -0400

    Redid overworld soundscape

--- Assets/Overworld/caveTeleporter.cs
+++ Assets/Overworld/caveTeleporter.cs
@@ -5,15 +5,6 @@ using UnityEngine;
 public class caveTeleporter : MonoBehaviour
 {
     [SerializeField]
-    private AudioSource backgroundMusic;
-
-    [SerializeField]
-    private AudioClip backgroundAudioclip;
-
-    [SerializeField]
-    private float musicVolume;
-
-    [SerializeField]
     private Transform teleportTransform;
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -21,9 +12,6 @@ public class caveTeleporter : MonoBehaviour
         var teleportLocation = teleportTransform.position;
         if (collision.gameObject.CompareTag("Player"))
         {
-            backgroundMusic.clip = backgroundAudioclip;
-            backgroundMusic.volume = musicVolume;
-            backgroundMusic.Play();
             parrot p = FindObjectOfType<parrot>();
             if (p)
             {
