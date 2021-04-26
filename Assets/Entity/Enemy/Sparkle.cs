d8071e2f4fe325f3310aa012967afa5cababdaa4
Tom Yarrow <tyarrow1@gmail.com>
2021-04-26 00:28:29 -0400

    Sparkles for elite enemies (WIP)

---
+++ Assets/Entity/Enemy/Sparkle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sparkle : MonoBehaviour
+{
+    public float lifetime;
+    private float killtime;
+
+    private void Start()
+    {
+        killtime = Time.time + lifetime;
+    }
+
+    void Update()
+    {
+        if (Time.time > killtime)
+        {
+            DestroyImmediate(this);
+        }
+    }
+}
---
+++ Assets/Entity/Enemy/SpawnSparkle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSparkle : MonoBehaviour
+{
+    public GameObject sparkle;
+    public Color sparkleColor;
+    public float rate;
+    public float size;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
