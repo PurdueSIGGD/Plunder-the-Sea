3aa2e3fc20447a4f64a9df6c085048f1f0feac7f
Andrew Choi <kimchy101@gmail.com>
2021-04-28 19:32:10 -0400

    tutorial revision additions for target

---
+++ Assets/TargetEntity.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TargetEntity : MonoBehaviour
+{
+    [SerializeField]
+    private int hitCountMax;
+    private int hitCount = 0;
+    public UnityEvent deathEvent;
+
+    private bool idle = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        if (idle) return;
+
+        if (collider.GetComponent<Projectile>() != null) {
+            hitCount += 1;
+        }
+
+        if (hitCount >= hitCountMax) {
+            //Destroy(gameObject);
+            GetComponent<SpriteRenderer>().enabled = false;
+            idle = true;
+            deathEvent.Invoke();
+            Invoke("revive", 2f);
+        }
+    }
+
+    void revive() {
+        idle = false;
+        GetComponent<SpriteRenderer>().enabled = true;
+        hitCount = 0;
+    }
+}
---
+++ Assets/TutorialAimingProgression.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TutorialAimingProgression : MonoBehaviour
+{
+
+    [SerializeField]
+    private int counterMax = 1;
+    private int counter = 0;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public UnityEvent counterComplete;
+
+    public void Increment() {
+        counter++;
+
+        if (counter >= counterMax) {
+            this.enabled = false;
+            counterComplete.Invoke();
+        }
+    }
+}
